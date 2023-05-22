1de8174e6101edb236cc9222cce3af69701977fd
Ferhat <ferhatmete9224@gmail.com>
2023-05-22 16:50:14 +0300

    Movies component ready

---
+++ src/app/movie.ts
@@ -0,0 +1,10 @@
+export class Movie {
+  id: number;
+  name: string;
+  static component: any;
+
+  constructor() {
+    this.id = 0;
+    this.name = '';
+  }
+}
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Movie } from '../movie';
 
 @Component({
   selector: 'movies',
-  templateUrl: './movies.component.html',
+  templateUrl: "movies.component.html",
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent {
+  title = 'Movie List'
 
+  movie: Movie= {
+    id: 1,
+    name: "Movie"
+  }
+
+  getTitle() {
+    return this.title;
+  }
 }
